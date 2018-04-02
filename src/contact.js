4a2e18ee79893fcdea98a4bbac6eb6334af86367
LachlanKI <lachlanki@gmail.com>
2018-04-02 16:18:34 +0100

    by

--- src/contact.js
+++ src/contact.js
@@ -12,7 +12,8 @@ export default class Contact extends React.Component {
             <address>
                 <a href="mailto:nordhoy@gmail.com?Subject=hello" target="_top">nordhoy@gmail.com</a>
                 <p>+47 959 77 386</p>
-                <a href="https://www.instagram.com/kristinnordhoy/?hl=en" target="_blank">instagram</a>
+                <a id="insta" href="https://www.instagram.com/kristinnordhoy/?hl=en" target="_blank">instagram</a>
+                <a id="yuk" href="http://yuk.digital" target="_blank">website by yuk</a>
             </address>
         )
     }
